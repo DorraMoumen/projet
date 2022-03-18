4c71e654f00fd350b18033205b5af7344b7e83de
DorraMoumen <dmoumen011@gmail.com>
2022-03-18 12:06:32 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import {Card,Button,Carousel} from 'react-bootstrap';
+import avisto from './assets/avisto.jpg'
+import NavPage from './components/NavPage';
+function App() {
+  return (
+    <div className="App">
+     <h1 style={{backgroundColor: "lightblue"}}>hello world</h1>
+
+<NavPage name={"mohamed"}/>
+    </div>
+  );
+}
+
+export default App;
